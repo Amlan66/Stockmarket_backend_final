177abde0a01c24338f32f448b8760fa9bc3687bf
Amlan66 <36122995+Amlan66@users.noreply.github.com>
2020-09-08 23:06:32 +0530

    Add files via upload

---
+++ src/main/java/com/wellsfargo/stockmarket/loginsignup/service/UserService.java
@@ -0,0 +1,99 @@
+package com.wellsfargo.stockmarket.loginsignup.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.wellsfargo.stockmarket.loginsignup.entity.User;
+import com.wellsfargo.stockmarket.loginsignup.repository.UserRepository;
+
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+
+@Service
+public class UserService {
+	@Autowired
+	UserRepository userRepository;
+	User user;
+	String res;
+
+	public String findbyID(String username, String password) {
+
+		user = userRepository.findByUserName(username);
+		if (user != null) {
+
+			if (user.getPassword().equals(password)) {
+
+				res = "login succesful";
+				return res;
+			} else {
+
+				res = "wrong password";
+				return res;
+
+			}
+
+		} else {
+
+			res = "USER NOT PRESENT";
+			return res;
+
+		}
+
+	}
+
+	public User addUser(String username, String password, String email) {
+		
+
+		user = userRepository.findByUserName(username);
+
+		if (user == null) {
+			user = userRepository.findByEmail(email);
+			if (user == null) {
+				User user = new User(username, password, email, "user");
+				user.setVerified("Y");
+
+				res = "USER ADDED";
+				return userRepository.save(user);
+			} else
+
+			{
+
+				res = "EMAIL ALREADY EXISTS";
+				return null;
+			}
+		} else {
+
+			res = "USER ALREADY PRESENT";
+			return null;
+		}
+
+	}
+
+	public User updateUser(String username, String password, String email) {
+
+		user = userRepository.findByUserName(username);
+
+		if (user != null && user.getUserName().equals(username)) {
+			user.setEmail(email);
+			user.setPassword(password);
+
+			return userRepository.save(user);
+
+		} else {
+
+			res = " USER NOT PRESENT";
+			return null;
+		}
+
+	}
+
+}
